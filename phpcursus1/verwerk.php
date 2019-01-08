a6bada6d784f5d3fccd4518e0ced3e6eb73460c2
beetlelaaf <33521516+beetlelaaf@users.noreply.github.com>
2019-01-08 10:49:42 +0100

    Still not working :exclamation:

--- phpcursus1/verwerk.php
+++ phpcursus1/verwerk.php
@@ -10,19 +10,26 @@
     <table>
         <?php
         
-        if(isset($_POST['naam']) && $_POST['naam'] != '') {
-          $naam = filter_var($_POST['naam'], FILTER_SANITIZE_STRING);
-          echo "<tr><td>Naam: </td><td>" . $naam . "</td></tr>";
+        //Processing the name input / output name
+        if(isset($_POST['naam']) &&  filter_var($_POST['naam'], FILTER_SANITIZE_STRING) ) { $naam = $_POST['naam'];
+          echo "<tr><td>Naam: </td><td>" . $_POST['naam'] . "</td></tr>\n";
         } else {
-          echo "<tr><td>Naam: </td><td><em>Vul een naam in!</em></td></tr>";
-        }
-        if ( isset($_POST['email'])  && filter_var($_POST['email'],  FILTER_VALIDATE_EMAIL)) {
-        echo "<tr><td>E-mail: </td><td>" . $_POST['email'] . "</td></tr>";
-        } else {
-        echo "<tr><td>E-mail: </td><td><em>Geen geldige E-mail!</em></td></tr>"; 
+          echo "<tr><td>Naam: </td><td><em>Vul een naam in!</em></td></tr>\n";
         }
 
-        echo "<tr><td>Reactie: </td><td>" . $_POST['reactie'] . "</td></tr>";
+        //processing the e-mail input / output e-meail
+        if (isset($_POST['email'])  && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) { $email = $_POST['email'];
+        echo "<tr><td>E-mail: </td><td>" . $_POST['email'] . "</td></tr>\n";
+        } else {
+        echo "<tr><td>E-mail: </td><td><em>Geen geldige E-mail!</em></td></tr>\n"; 
+        }
+
+        //output reactie
+        if(isset($_POST['reactie']) &&  filter_var($_POST['reactie'], FILTER_SANITIZE_STRING) ) { $reactie = ($_POST['reactie']);
+            echo "<tr><td>Reactie: </td><td>" . $_POST['reactie'] . "</td></tr>\n";
+        } else {
+            echo "<tr><td>Reactie: </td><td><em>Vul een reactie in!</em></td></tr>\n";
+        }
 
         ?>
     </table>
