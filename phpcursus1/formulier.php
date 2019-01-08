3922e60b6a4c3bc03709e88d184053f8671ab54c
beetlelaaf <33521516+beetlelaaf@users.noreply.github.com>
2019-01-08 11:23:18 +0100

    Still Doesn't Work :exclamation:

--- phpcursus1/formulier.php
+++ phpcursus1/formulier.php
@@ -1,34 +1,35 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <title>Formulier</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <form
+            name="reactie formulier" method="post" action="verwerk.php">
+            <table width="300" border="0">
+                <tbody>
+                    <tr>
+                        <td>Naam:</td>
+                        <td><input type="text" name="naam"></td>
+                    </tr>
 
-<head>
-    <meta charset="utf-8" />
-    <title>PHP cursus 1</title>
-</head>
+                    <tr>
+                        <td>E-mail:</td>
+                        <td><input type="text" name="email"></td>
+                    </tr>
 
-<body>
-    <form name="reactie formulier" method="post" action="verwerk.php">
-        <table width="300">
-            <tbody>
-                <tr>
-                    <td>Naam:</td>
-                    <td><input type="text" name="naam"></td>
-                </tr>
-                <tr>
-                    <td>Email:</td>
-                    <td><input type="text" name="email"></td>
-                </tr>
-                <tr>
-                    <td>Reactie:</td>
-                    <td><textarea name="reactie" rows="8" cols="43"></textarea></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><input type="submit" value="verstuur"></td>
-                </tr>
-            </tbody>
-        </table>
-    </form>
-</body>
+                    <tr>
+                        <td>Reactie:</td>
+                        <td><textarea name="reactie" rows="8" cols="43"></textarea></td>
+                    </tr>
 
+                    <tr>
+                        <td></td>
+                        <td><input type="submit" value="verstuur"></td>
+                    </tr>
+            </table>
+        </form>
+    </body>
 </html>
--- phpcursus1/verwerk.php
+++ phpcursus1/verwerk.php
@@ -1,37 +1,26 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8" />
-    <title>Formulier verwerken</title>
-</head>
-
-<body>
-    <table>
-        <?php
-        
-        //Processing the name input / output name
-        if(isset($_POST['naam']) &&  filter_var($_POST['naam'], FILTER_SANITIZE_STRING) ) { $naam = $_POST['naam'];
-          echo "<tr><td>Naam: </td><td>" . $_POST['naam'] . "</td></tr>\n";
-        } else {
-          echo "<tr><td>Naam: </td><td><em>Vul een naam in!</em></td></tr>\n";
-        }
-
-        //processing the e-mail input / output e-meail
-        if (isset($_POST['email'])  && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) { $email = $_POST['email'];
-        echo "<tr><td>E-mail: </td><td>" . $_POST['email'] . "</td></tr>\n";
-        } else {
-        echo "<tr><td>E-mail: </td><td><em>Geen geldige E-mail!</em></td></tr>\n"; 
-        }
-
-        //output reactie
-        if(isset($_POST['reactie']) &&  filter_var($_POST['reactie'], FILTER_SANITIZE_STRING) ) { $reactie = ($_POST['reactie']);
-            echo "<tr><td>Reactie: </td><td>" . $_POST['reactie'] . "</td></tr>\n";
-        } else {
-            echo "<tr><td>Reactie: </td><td><em>Vul een reactie in!</em></td></tr>\n";
-        }
-
-        ?>
-    </table>
-</body>
+<html> 
+    <head> 
+        <title>Formulier verwerken</title> 
+    </head> 
+    <body>
+        <table> 
+            <?php 
+            if (isset($_POST["naam"]) && filter_var($_POST["naam"], FILTER_SANITIZE_STRING) ) { $naam = $_POST["naam"];
+            echo "<tr><td>Naam: </td><td>" . $_POST["naam"]  . "</td></tr>\n";}
+            else {
+                echo "<tr><td>Naam: </td><td><em>Vul een naam in!</em></td></tr>\n";
+            }
+            if (isset($_POST["email"]) && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) ) { $email = $_POST["naam"];
+            echo "<tr><td>Email: </td><td>" . $_POST["email"]  . "</td></tr>\n";}
+            else {
+                echo "<tr><td>Email: </td><td><em>Vul een email in!</em></td></tr>\n";
+            }
+            if (isset($_POST["reactie"]) && filter_var($_POST["reactie"], FILTER_SANITIZE_STRING) )  { $reactie = $_POST["reactie"];
+            echo "<tr><td>Reactie: </td><td>" . $_POST["reactie"]  . "</td></tr>\n";}
+            else {
+                echo "<tr><td>Reactie: </td><td><em>Vul een reactie in!</em></td></tr>\n";
+            }
+            ?> 
+        </table> 
+    </body> 
 </html>
